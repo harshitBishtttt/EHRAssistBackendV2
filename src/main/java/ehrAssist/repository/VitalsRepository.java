1e75e0417e246f505673e55e3491bab81b79637a
Harshit <Harshit@SEZLAP-18771.india.rsystems.com>
2026-04-17 12:32:40 +0530

    re

--- src/main/java/ehrAssist/repository/VitalsRepository.java
+++ src/main/java/ehrAssist/repository/VitalsRepository.java
@@ -12,10 +12,16 @@ import java.util.UUID;
 @Repository
 public interface VitalsRepository extends JpaRepository<VitalsEntity, UUID> {
 
-    @Query(value = "SELECT v.* FROM vitals v " +
-            "INNER JOIN observation_code_master ocm ON v.observation_code_id = ocm.item_id " +
+    @Query(value = "SELECT v.* " +
+            "FROM vitals AS v " +
+            "INNER JOIN observation_code_master AS ocm ON v.observation_code_id = ocm.item_id " +
             "WHERE (:patientId IS NULL OR v.patient_id = :patientId) " +
             "AND (:loincCode IS NULL OR :loincCode = '' OR ocm.loinc_code = :loincCode)",
+            countQuery = "SELECT COUNT(1) " +
+                    "FROM vitals AS v " +
+                    "INNER JOIN observation_code_master AS ocm ON v.observation_code_id = ocm.item_id " +
+                    "WHERE (:patientId IS NULL OR v.patient_id = :patientId) " +
+                    "AND (:loincCode IS NULL OR :loincCode = '' OR ocm.loinc_code = :loincCode)",
             nativeQuery = true)
     Page<VitalsEntity> searchVitals(UUID patientId, String loincCode, Pageable pageable);
 }
